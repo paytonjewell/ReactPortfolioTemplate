90dba18fea28f6cb62032634fd259eb4153e59a9
Payton Jewell <paytontaylor96@gmail.com>
2020-03-16 17:03:12 -0500

    added hire.html, modified other pages

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ window.onscroll = function() {
         navbar.classList.remove("scrolled");
     }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
