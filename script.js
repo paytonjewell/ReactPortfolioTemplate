73ff3361a76e99fb33eac893948810a3b53d935f
Payton Jewell <paytontaylor96@gmail.com>
2020-03-17 15:29:43 -0500

    v 2.0

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-window.onscroll = function() {
-    var navbar = document.getElementById('navigation');
-    if ( window.pageYOffset > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-}
+$(document).ready(function() {
+  $("#techfolder").click(function(){
+    $("#txt").toggleClass("d-none");
+  });
+  $("#persfolder").click(function(){
+    $("#txt2").toggleClass("d-none");
+  });
+  $("#linkfolder").click(function(){
+    $("#txt3").toggleClass("d-none");
+  });
+});
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
+
+window.onscroll = function() {
+  var navbar = document.getElementById('navigation');
+  if ( window.pageYOffset > 50) {
+      navbar.classList.add("scrolled");
+  } else {
+      navbar.classList.remove("scrolled");
+  }
+}
