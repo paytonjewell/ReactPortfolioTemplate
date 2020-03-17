1ef7e77ba21bda61fcea0a2cc8f25746d0004438
Payton Jewell <paytontaylor96@gmail.com>
2020-03-17 16:35:14 -0500

    made more responsive

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
   });
 });
 
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
