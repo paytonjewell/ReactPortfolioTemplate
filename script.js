2cd8d303c85077a251e2521f28ba1e36fb0e6607
Payton Jewell <paytontaylor96@gmail.com>
2020-06-30 10:48:55 -0500

    updated javascript to make file visibility on hiring page exclusive

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
   $("#techfolder").click(function(){
     $("#txt").toggleClass("d-none");
+    if (! $("#txt2").hasClass('d-none')) {
+      $("#txt2").toggleClass("d-none");
+    }
+    if (! $("#txt3").hasClass('d-none')) {
+      $("#txt3").toggleClass("d-none");
+    }
   });
   $("#persfolder").click(function(){
     $("#txt2").toggleClass("d-none");
+    if (! $("#txt").hasClass('d-none')) {
+      $("#txt").toggleClass("d-none");
+    }
+    if (! $("#txt3").hasClass('d-none')) {
+      $("#txt3").toggleClass("d-none");
+    }
   });
   $("#linkfolder").click(function(){
     $("#txt3").toggleClass("d-none");
+    if (! $("#txt2").hasClass('d-none')) {
+      $("#txt2").toggleClass("d-none");
+    }
+    if (! $("#txt").hasClass('d-none')) {
+      $("#txt").toggleClass("d-none");
+    }
   });
 });
 
