9a30d5fe18c7e1ad28e7e006ab6217d7e3df0788
Payton Jewell <paytontaylor96@gmail.com>
2020-07-17 06:14:01 -0500

    completely revamped homepage

--- script.js
+++ script.js
@@ -1,49 +1,13 @@
-$(document).ready(function() {
-  $("#techfolder").click(function(){
-    $("#txt").toggleClass("d-none");
-    if (! $("#txt2").hasClass('d-none')) {
-      $("#txt2").toggleClass("d-none");
-    }
-    if (! $("#txt3").hasClass('d-none')) {
-      $("#txt3").toggleClass("d-none");
-    }
-  });
-  $("#persfolder").click(function(){
-    $("#txt2").toggleClass("d-none");
-    if (! $("#txt").hasClass('d-none')) {
-      $("#txt").toggleClass("d-none");
-    }
-    if (! $("#txt3").hasClass('d-none')) {
-      $("#txt3").toggleClass("d-none");
-    }
-  });
-  $("#linkfolder").click(function(){
-    $("#txt3").toggleClass("d-none");
-    if (! $("#txt2").hasClass('d-none')) {
-      $("#txt2").toggleClass("d-none");
-    }
-    if (! $("#txt").hasClass('d-none')) {
-      $("#txt").toggleClass("d-none");
-    }
-  });
-});
+/* dark mode toggle */
 
+const container = document.querySelector('#container');
+const toggler = document.querySelector('.toggler');
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
-
-window.onscroll = function() {
-  var navbar = document.getElementById('navigation');
-  if ( window.pageYOffset > 50) {
-      navbar.classList.add("scrolled");
-  } else {
-      navbar.classList.remove("scrolled");
+toggler.addEventListener('click', () => {
+  container.classList.toggle('dark');
+  if (container.classList.contains('dark')) {
+    toggler.innerHTML = "🌕";
+  } else if (!container.classList.contains('dark')) {
+    toggler.innerHTML = "🌑";
   }
-}
+});
