23643603ff019e341dd0359035dd785f9a39f0c6
Payton Jewell <paytontaylor96@gmail.com>
2020-07-18 17:32:44 -0500

    created more pages, configured some things, changed footer design

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ toggler.addEventListener('click', () => {
     toggler.innerHTML = "🌑";
   }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
