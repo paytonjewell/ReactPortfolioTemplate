d6751c59aa22328eeeecce2e9ce393b15569fea4
Payton Jewell <paytonjewell@macbook-pro.home>
2020-09-09 11:38:55 -0500

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, {createContext} from 'react';
 import Style from './App.module.scss';
 import BaseLayout from "./Components/BaseLayout";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Home from "./Components/Home";
 
-
-const { Provider, Consumer } = createContext({
-    darkmode: {}
-})
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
           <div className={Style.app}>
               <BaseLayout>
                   <Home />
               </BaseLayout>
           </div>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
