58273b95b348adca994dcc280fcf608c4c1a6a06
Payton Jewell <paytontaylor96@gmail.com>
2020-09-25 14:58:34 -0500

    updated broken links

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -49,23 +49,23 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            liveLink: "#", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            sourceLink: "#", // this should be a link to the **repository** of the project, where the code is hosted. Github, Phabricator, etc.
+            liveLink: "paytonjewell.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            sourceLink: "paytonjewell.dev", // this should be a link to the **repository** of the project, where the code is hosted. Github, Phabricator, etc.
         },
         {
             title: "Project 2",
-            liveLink: "#",
-            sourceLink: "#",
+            liveLink: "paytonjewell.dev",
+            sourceLink: "paytonjewell.dev",
         },
         {
             title: "Project 3",
-            liveLink: "#",
-            sourceLink: "#",
+            liveLink: "paytonjewell.dev",
+            sourceLink: "paytonjewell.dev",
         },
         {
             title: "Project 4",
-            liveLink: "#",
-            sourceLink: "#",
+            liveLink: "paytonjewell.dev",
+            sourceLink: "paytonjewell.dev",
         },
     ]
 }
