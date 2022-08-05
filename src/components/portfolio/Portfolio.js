3f1b998685f56a5863b02ae314c77f6344b4195d
Igor Nascimento <igor.f.nascimento@accenture.com>
2022-08-05 01:20:06 -0300

    dev: protifolio TDB

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,11 +7,12 @@ export default function Portfolio() {
     return (
         <Box>
             <Grid container display={'flex'} justifyContent={'center'}>
-                {info.portfolio.map((project, index) => (
+                {/* {info.portfolio.map((project, index) => (
                    <Grid item xs={12} md={6} key={index}>
                        <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
                    </Grid>
-                ))}
+                ))} */}
+                <h1>TDB</h1>
             </Grid>
         </Box>
     );
