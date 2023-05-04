a9728ef59e8df99d252d1890753096c982b16bdf
Oscar Gallego <hola@oscargallegoruiz.com>
2023-05-04 09:15:53 +0200

    Remove data info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -13,16 +13,10 @@ These colors will be used to style your name on the homepage, the background of 
 the site.
  */
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
-
 export const info = {
   firstName: "Óscar",
   lastName: "Gallego",
-  initials: "OG", // the example uses first and last, but feel free to use three or more if you like.
+  initials: "OGR",
   position: "a Frontend Developer",
   selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
   gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
