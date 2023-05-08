eb29d8bade1ad57c3430b33f27c8a008131b2f3b
Oscar <77354899+garbarok@users.noreply.github.com>
2023-05-08 12:41:22 +0200

    Remove data info.js (#1)
    Co-authored-by: Oscar Gallego <hola@oscargallegoruiz.com>

--- src/info/Info.js
+++ src/info/Info.js
@@ -2,6 +2,7 @@ import self from "../img/self.png";
 import mock1 from "../img/mock1.png";
 import mock2 from "../img/mock2.png";
 import mock3 from "../img/mock3.png";
+import mock4 from "../img/mock4.png";
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -13,16 +14,10 @@ These colors will be used to style your name on the homepage, the background of 
 the site.
  */
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
-
 export const info = {
   firstName: "Óscar",
   lastName: "Gallego",
-  initials: "OG", // the example uses first and last, but feel free to use three or more if you like.
+  initials: "OGR",
   position: "a Frontend Developer",
   selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
   gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
@@ -87,13 +82,19 @@ export const info = {
   portfolio: [
     // This is where your portfolio projects will be detailed
     {
-      title: "Full Stack App",
+      title: "Weather App (Vue)",
+      live: "https://weatherapp.oscargallegoruiz.com/",
+      source: "https://github.com/garbarok/weather-app",
+      image: mock4,
+    },
+    {
+      title: "Full Stack App (React, Next.js, PostgreSQL, Primsa)",
       live: "https://fullstack.garbarok.com", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
       source: "https://github.com/garbarok/fullstack-app", // this should be a link to the **repository** of the project, where the code is hosted.
       image: mock1,
     },
     {
-      title: "AdoptMe",
+      title: "AdoptMe (React)",
       live: "https://adopt-me-red.vercel.app/",
       source: "https://github.com/garbarok/AdoptMe",
       image: mock2,
