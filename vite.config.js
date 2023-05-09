42ac330baa62df64ed5884593a209fe30d4a63cb
Oscar Gallego <hola@oscargallegoruiz.com>
2023-05-09 20:42:54 +0200

    change output directory vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     build: {
-      outDir: "build",
+      outDir: "dist",
     },
     plugins: [react()],
   };
